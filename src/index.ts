9ac2562a0febb20d5bceeebbc6a75b65ab041a9c
Liad Uziel <liad.uz@gmail.com>
2024-11-15 12:31:57 +0200

    Changing the code so that it reads according to arguments

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,17 @@ import { GameHelper } from "./gameHelper.js";
 import { PlayerType } from "./types/playerType.type.js";
 
 const run = async () => {
-  const numberOfHands = Number(
-    GameHelper.getValueFromArgs("numberOfHands") || 3
-  );
-  const numberOfRounds = Number(
-    GameHelper.getValueFromArgs("numberOfRounds") || 2
-  );
+  const inputArgHands = GameHelper.getValueFromArgs("numberOfHands");
+  const numberOfHands = GameHelper.validatePositiveNumber(inputArgHands, 3);
+
+  const inputArgRounds = GameHelper.getValueFromArgs("numberOfRounds");
+  const numberOfRounds = GameHelper.validatePositiveNumber(inputArgRounds, 3);
 
   const player1Type: PlayerType =
-    GameHelper.getValueFromArgs(`player1Type`) ||
+    GameHelper.validatePlayerType(GameHelper.getValueFromArgs(`player1Type`)) ||
     (await GameHelper.choosePlayerType("Choose player 1 type"));
   const player2Type: PlayerType =
-    GameHelper.getValueFromArgs(`player2Type`) ||
+    GameHelper.validatePlayerType(GameHelper.getValueFromArgs(`player2Type`)) ||
     (await GameHelper.choosePlayerType("Choose player 2 type"));
 
   await startGame(player1Type, player2Type, numberOfRounds, numberOfHands);
