9367a49847362752cd9a917a8f7d503cf276579d
Liad Uziel <liad.uz@gmail.com>
2024-11-15 13:09:41 +0200

    Adjust the index.ts to the new Game class

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { determineWinnerWithScoreboard } from "./determineWinner.js";
+import { Game } from "./game.js";
 import { GameHelper } from "./gameHelper.js";
 import { PlayerType } from "./types/playerType.type.js";
 
@@ -7,7 +7,7 @@ const run = async () => {
   const numberOfHands = GameHelper.validatePositiveNumber(inputArgHands, 3);
 
   const inputArgRounds = GameHelper.getValueFromArgs("numberOfRounds");
-  const numberOfRounds = GameHelper.validatePositiveNumber(inputArgRounds, 3);
+  const numberOfRounds = GameHelper.validatePositiveNumber(inputArgRounds, 2);
 
   const player1Type: PlayerType =
     GameHelper.validatePlayerType(GameHelper.getValueFromArgs(`player1Type`)) ||
@@ -16,38 +16,13 @@ const run = async () => {
     GameHelper.validatePlayerType(GameHelper.getValueFromArgs(`player2Type`)) ||
     (await GameHelper.choosePlayerType("Choose player 2 type"));
 
-  await startGame(player1Type, player2Type, numberOfRounds, numberOfHands);
-
-  console.log("Thanks for playing! Goodbye.");
-};
-
-const startGame = async (
-  player1Type: PlayerType,
-  player2Type: PlayerType,
-  numberOfRounds: number,
-  numberOfHands: number
-) => {
-  const [player1, player2] = GameHelper.buildPlayersByType(
+  const game = new Game(
     player1Type,
-    player2Type
+    player2Type,
+    numberOfRounds,
+    numberOfHands
   );
-
-  for (let index = 0; index < numberOfRounds; index++) {
-    console.log(`Round ${index + 1}/${numberOfRounds}:`);
-
-    if (player1Type === "Human") {
-      console.log("Player 1:");
-    }
-    const p1Hands = await player1.getHands(numberOfHands);
-
-    if (player2Type === "Human") {
-      console.log("Player 2:");
-    }
-    const p2Hands = await player2.getHands(numberOfHands);
-
-    const scoreboard = determineWinnerWithScoreboard(p1Hands, p2Hands);
-    console.log("SCOREBOARD:", scoreboard);
-  }
+  await game.start();
 };
 
 run();
