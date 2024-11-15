e342e75924787dd4e656568e97ca3e6c899c3e36
Liad Uziel <liad.uz@gmail.com>
2024-11-15 13:44:48 +0200

    Option for a monkey player with the use of the Strategy design pattern

--- src/players/cpuPlayer.ts
+++ src/players/cpuPlayer.ts
@@ -1,12 +1,38 @@
 import { GameHelper } from '../gameHelper.js';
+import { CpuPlayerStrategy } from '../types/cpuPlayerStrategy.type.js';
 import { Hand } from '../types/hand.type.js';
 
 export class CpuPlayer {
+  private strategy: CpuPlayerStrategy;
+
+  constructor(strategy: CpuPlayerStrategy = 'random') {
+    this.strategy = strategy;
+  }
+
+  /**
+   * Switch between strategies
+   * @param strategy
+   */
+  setStrategy(strategy: CpuPlayerStrategy) {
+    this.strategy = strategy;
+  }
+
+  /**
+   * Get hands based on the strategy
+   *
+   * @param numberOfHands
+   */
   getHands = (numberOfHands: number) => {
     const hands: Hand[] = [];
-    for (let index = 0; index < numberOfHands; index++) {
-      const hand = GameHelper.chooseRandomHand();
-      hands.push(hand);
+    if (this.strategy === 'monkey') {
+      // Monkey strategy: Draw one random hand per round
+      hands.push(GameHelper.chooseRandomHand());
+    } else {
+      // Random strategy: Draw a number of random hands
+      for (let index = 0; index < numberOfHands; index++) {
+        const hand = GameHelper.chooseRandomHand();
+        hands.push(hand);
+      }
     }
     return hands;
   };
